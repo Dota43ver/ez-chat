05851ae740c7cbc1cef2af88d9899b97d158ff2b
Dota43ver <facundoagueroc@gmail.com>
2024-04-26 23:31:28 -0300

    styles

--- src/components/vector/vector.jsx
+++ src/components/vector/vector.jsx
@@ -26,7 +26,25 @@ const Detail = () => {
             <span>Shared photos</span>
             <img src="arrowDown.png" alt="" />
           </div>
-         
+          <div className="photos">
+            <div className="photoItem">
+              <div className="photoDetail">  
+              <img src="https://media.istockphoto.com/id/1793397465/es/foto/de-remo.jpg?s=2048x2048&w=is&k=20&c=JaPmzn5BtA0Wu2Zj0aDP672lu6e9jjWTkTIks2ZYOpM=" alt="" />
+              <span>photo_2024_2.png</span>
+              </div>
+            </div>
+            <img src="./download.png" alt="" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared Files</span>
+            <img src="arrowUp.png" alt="" />
+          </div>
+        </div>
+        <button>Block User</button>
+      </div>
+    </div>
   )
 }
 
