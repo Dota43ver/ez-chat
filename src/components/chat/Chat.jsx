f0fc894f2ff41b9428672bc862e792d6d5012d85
Dota43ver <facundoagueroc@gmail.com>
2024-04-30 18:34:37 -0300

    notifications

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,10 +1,10 @@
-import { useEffect, useRef, useState } from "react"
-import "./chat.css"
-import EmojiPicker from "emoji-picker-react"
+import { useEffect, useRef, useState } from "react";
+import "./chat.css";
+import EmojiPicker from "emoji-picker-react";
 
 const Chat = () => {
-  const [open,setOpen] = useState(false)
-  const [text, setText] = useState("")
+  const [open, setOpen] = useState(false);
+  const [text, setText] = useState("");
 
   const endRef = useRef(null);
 
@@ -12,33 +12,51 @@ const Chat = () => {
     endRef.current?.scrollIntoView({ behavior: "smooth" });
   }, []);
 
-  const handleEmoji = e =>{
-    setText(prev=>prev+e.emoji)
-    setOpen(false)
-  }
+  const handleEmoji = (e) => {
+    setText((prev) => prev + e.emoji);
+    setOpen(false);
+  };
 
   return (
     <div className="chat">
       <div className="top">
         <div className="user">
-          <img src="./avatar.png" alt="" />
+          <img
+            src="./avatar.png"
+            alt=""
+          />
           <div className="texts">
             <span>Jane Doe</span>
             <p>Lorem ipsum dolor sit amet.</p>
           </div>
         </div>
         <div className="icons">
-          <img src="./phone.png" alt="" />
-          <img src="./video.png" alt="" />
-          <img src="./info.png" alt="" />
+          <img
+            src="./phone.png"
+            alt=""
+          />
+          <img
+            src="./video.png"
+            alt=""
+          />
+          <img
+            src="./info.png"
+            alt=""
+          />
         </div>
       </div>
       <div className="center">
         <div className="message">
-          <img src="avatar.png" alt="" />
+          <img
+            src="avatar.png"
+            alt=""
+          />
           <div className="texts">
             <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium commodi placeat dolores repellat quia ab quas, labore esse rerum vitae dolor vel expedita quis quisquam sit in. Obcaecati, commodi accusamus.
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Praesentium commodi placeat dolores repellat quia ab quas, labore
+              esse rerum vitae dolor vel expedita quis quisquam sit in.
+              Obcaecati, commodi accusamus.
             </p>
             <span>1 min ago</span>
           </div>
@@ -46,16 +64,25 @@ const Chat = () => {
         <div className="message own">
           <div className="texts">
             <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium commodi placeat dolores repellat quia ab quas, labore esse rerum vitae dolor vel expedita quis quisquam sit in. Obcaecati, commodi accusamus.
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Praesentium commodi placeat dolores repellat quia ab quas, labore
+              esse rerum vitae dolor vel expedita quis quisquam sit in.
+              Obcaecati, commodi accusamus.
             </p>
             <span>1 min ago</span>
           </div>
         </div>
         <div className="message">
-          <img src="avatar.png" alt="" />
+          <img
+            src="avatar.png"
+            alt=""
+          />
           <div className="texts">
             <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium commodi placeat dolores repellat quia ab quas, labore esse rerum vitae dolor vel expedita quis quisquam sit in. Obcaecati, commodi accusamus.
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Praesentium commodi placeat dolores repellat quia ab quas, labore
+              esse rerum vitae dolor vel expedita quis quisquam sit in.
+              Obcaecati, commodi accusamus.
             </p>
             <span>1 min ago</span>
           </div>
@@ -63,25 +90,40 @@ const Chat = () => {
         <div className="message own">
           <div className="texts">
             <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium commodi placeat dolores repellat quia ab quas, labore esse rerum vitae dolor vel expedita quis quisquam sit in. Obcaecati, commodi accusamus.
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Praesentium commodi placeat dolores repellat quia ab quas, labore
+              esse rerum vitae dolor vel expedita quis quisquam sit in.
+              Obcaecati, commodi accusamus.
             </p>
             <span>1 min ago</span>
           </div>
         </div>
         <div className="message">
-          <img src="avatar.png" alt="" />
+          <img
+            src="avatar.png"
+            alt=""
+          />
           <div className="texts">
             <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium commodi placeat dolores repellat quia ab quas, labore esse rerum vitae dolor vel expedita quis quisquam sit in. Obcaecati, commodi accusamus.
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Praesentium commodi placeat dolores repellat quia ab quas, labore
+              esse rerum vitae dolor vel expedita quis quisquam sit in.
+              Obcaecati, commodi accusamus.
             </p>
             <span>1 min ago</span>
           </div>
         </div>
         <div className="message own">
           <div className="texts">
-            <img src="https://media.istockphoto.com/id/1793397465/es/foto/de-remo.jpg?s=2048x2048&w=is&k=20&c=JaPmzn5BtA0Wu2Zj0aDP672lu6e9jjWTkTIks2ZYOpM=" alt="" />
+            <img
+              src="https://media.istockphoto.com/id/1793397465/es/foto/de-remo.jpg?s=2048x2048&w=is&k=20&c=JaPmzn5BtA0Wu2Zj0aDP672lu6e9jjWTkTIks2ZYOpM="
+              alt=""
+            />
             <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium commodi placeat dolores repellat quia ab quas, labore esse rerum vitae dolor vel expedita quis quisquam sit in. Obcaecati, commodi accusamus.
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Praesentium commodi placeat dolores repellat quia ab quas, labore
+              esse rerum vitae dolor vel expedita quis quisquam sit in.
+              Obcaecati, commodi accusamus.
             </p>
             <span>1 min ago</span>
           </div>
@@ -90,21 +132,42 @@ const Chat = () => {
       </div>
       <div className="bottom">
         <div className="icons">
-          <img src="img.png" alt="" />
-          <img src="camera.png" alt="" />
-          <img src="mic.png" alt="" />
+          <img
+            src="img.png"
+            alt=""
+          />
+          <img
+            src="camera.png"
+            alt=""
+          />
+          <img
+            src="mic.png"
+            alt=""
+          />
         </div>
-        <input type="text" placeholder="Type a massage..." value={text} onChange={e=>setText(e.target.value)}/>
+        <input
+          type="text"
+          placeholder="Type a massage..."
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
         <div className="emoji">
-          <img src="./emoji.png" alt="" onClick={() => setOpen((prev) => !prev)}/>
+          <img
+            src="./emoji.png"
+            alt=""
+            onClick={() => setOpen((prev) => !prev)}
+          />
           <div className="picker">
-           <EmojiPicker open={open} onEmojiClick={handleEmoji}/>
+            <EmojiPicker
+              open={open}
+              onEmojiClick={handleEmoji}
+            />
           </div>
         </div>
         <button className="sendButton">Send</button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Chat
+export default Chat;
--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,48 +1,80 @@
-import { useState } from "react"
-import "./login.css"
+import { useState } from "react";
+import "./login.css";
+import { toast } from "react-toastify";
 
 const Login = () => {
+  const [avatar, setAvatar] = useState({
+    file: null,
+    url: "",
+  });
 
-    const [avatar, setAvatar] = useState({
-        file:null,
-        url:""
-    })
-
-    const handleAvatar = e =>{
-        if(e.target.files[0]){
-
-            setAvatar({
-                file:e.target.files[0],
-                url: URL.createObjectURL(e.target.files[0])
-            })
-        }
+  const handleAvatar = (e) => {
+    if (e.target.files[0]) {
+      setAvatar({
+        file: e.target.files[0],
+        url: URL.createObjectURL(e.target.files[0]),
+      });
     }
+  };
+
+  const handleLogin = e => {
+    e.preventDefault()
+    toast.warn("Hello")
+  }
 
   return (
     <div className="login">
-        <div className="item">
-            <h2>Welcome back,</h2>
-            <form>
-                <input type="text" placeholder="Email" name="email" />
-                <input type="password" placeholder="Password" name="password" />
-                <button>Sign in</button>
-            </form>
-        </div>
-        <div className="separator"></div>
-        <div className="item">
+      <div className="item">
+        <h2>Welcome back,</h2>
+        <form onSubmit={handleLogin}>
+          <input
+            type="text"
+            placeholder="Email"
+            name="email"
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+          />
+          <button>Sign in</button>
+        </form>
+      </div>
+      <div className="separator"></div>
+      <div className="item">
         <h2>Create an account</h2>
-            <form>
-                <img src={avatar.url || "./avatar.png"} alt="" />
-                <label htmlFor="file">Upload an image</label>
-                <input type="file" id="file"  style={{display:"none"}} onChange={handleAvatar}/>
-                <input type="text" placeholder="Username" name="username" />
-                <input type="text" placeholder="Email" name="email" />
-                <input type="password" placeholder="Password" name="password" />
-                <button>Sign Up</button>
-            </form>
-        </div>
+        <form>
+          <img
+            src={avatar.url || "./avatar.png"}
+            alt=""
+          />
+          <label htmlFor="file">Upload an image</label>
+          <input
+            type="file"
+            id="file"
+            style={{ display: "none" }}
+            onChange={handleAvatar}
+          />
+          <input
+            type="text"
+            placeholder="Username"
+            name="username"
+          />
+          <input
+            type="text"
+            placeholder="Email"
+            name="email"
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+          />
+          <button>Sign Up</button>
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
