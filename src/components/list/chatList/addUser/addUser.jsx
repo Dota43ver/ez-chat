1066bb3042a6e6b23df2ddc55a1c923a4adc11b2
Dota43ver <facundoagueroc@gmail.com>
2024-05-01 22:40:23 -0300

    fetching chats

--- src/components/list/chatList/addUser/addUser.jsx
+++ src/components/list/chatList/addUser/addUser.jsx
@@ -1,21 +1,73 @@
-import "./addUser.css"
+import { collection, doc, getDoc, getDocs, query, serverTimestamp, setDoc, where } from "firebase/firestore";
+import "./addUser.css";
+import { db } from "../../../../lib/firebase";
+import { useState } from "react";
 
 const AddUser = () => {
-  return (
-    <div className='addUser'>
-        <form>
-            <input type="text" placeholder="Username" name="username" />
-            <button>Search</button>
-        </form>
-        <div className="user">
-            <div className="detail">
-                <img src="./avatar.png" alt="" />
-                <span>Jane Doe</span>
-            </div>
-                <button>Add User</button>
-        </div>
-    </div>
-  )
-}
+  const [user, setUser] = useState(null)
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const username = formData.get("username");
 
-export default AddUser
+    try {
+      const userRef = collection(db, "users");
+
+      // Create a query against the collection.
+      const q = query(userRef, where("username", "==", username));
+
+      const querySnapShot = await getDocs(q)
+
+      if(!querySnapShot.empty)
+      setUser(querySnapShot.docs[0].data())
+
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleAdd = async() =>{
+
+    const chatRef = collection(db,"chats")
+    const userChatRef = collection(db,"userchats")
+
+    try {
+
+      const newChatRef = doc(chatRef)
+
+      await setDoc(newChatRef,{
+        createdAt: serverTimestamp(),
+        messages:[]
+      })
+
+      console.log(newChatRef.id)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  return (
+    <div className="addUser">
+      <form onSubmit={handleSearch}>
+        <input
+          type="text"
+          placeholder="Username"
+          name="username"
+        />
+        <button>Search</button>
+      </form>
+      {user  && <div className="user">
+        <div className="detail">
+          <img
+            src={user.avatar || "./avatar.png"}
+            alt=""
+          />
+          <span>{user.username}</span>
+        </div>
+        <button onClick={handleAdd}>Add User</button>
+      </div>}
+    </div>
+  );
+};
+
+export default AddUser;
