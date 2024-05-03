6b2c36deaa79130375f379e9e399f9d0097cae34
Dota43ver <facundoagueroc@gmail.com>
2024-05-03 12:55:00 -0300

    logout bug

--- src/components/list/userInfo/Userinfo.jsx
+++ src/components/list/userInfo/Userinfo.jsx
@@ -1,23 +1,36 @@
-import "./userInfo.css"
-import {useUserStore} from "../../../lib/userStore"
+import "./userInfo.css";
+import { useUserStore } from "../../../lib/userStore";
+import { auth } from "../../../lib/firebase";
 
 const Userinfo = () => {
-
-  const {currentUser} = useUserStore()
+  const { currentUser } = useUserStore();
 
   return (
-    <div className='userInfo'>
+    <div className="userInfo">
       <div className="user">
-        <img src={currentUser.avatar || "./avatar.png"} alt="" />
+        <img
+          src={currentUser.avatar || "./avatar.png"}
+          alt=""
+        />
         <h2>{currentUser.username}</h2>
+        <button onClick={() => auth.signOut()}>Logout</button>
       </div>
       <div className="icons">
-        <img src="./more.png" alt="" />
-        <img src="./video.png" alt="" />
-        <img src="./edit.png" alt="" />
+        <img
+          src="./more.png"
+          alt=""
+        />
+        <img
+          src="./video.png"
+          alt=""
+        />
+        <img
+          src="./edit.png"
+          alt=""
+        />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Userinfo
+export default Userinfo;
